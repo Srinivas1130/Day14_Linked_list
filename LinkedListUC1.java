9fe78aea5adaacac8e6fb4487cdb0300848b6d48
Srinivas1130 <srinivasachar1998@gmail.com>
2022-05-17 08:25:55 +0530

    Added code to store the data in node

---
+++ LinkedListUC1.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day14;
+
+import java.util.LinkedList;
+
+public class LinkedListUC1 {
+      public static void main(String []args){
+    	  LinkedList linkedList= new LinkedList();
+    	  linkedList.add(30);
+    	  linkedList.add(95);
+    	  linkedList.add(70);
+    	  System.out.println(linkedList);
+      }
+}
